1fb183a75ddceace707ccde2d654d643f8965460
Cole Puls <colepuls@me.com>
2025-05-11 15:45:17 -0500

    Added documentation for DreamCard.jsx

--- src/components/DreamCard.jsx
+++ src/components/DreamCard.jsx
@@ -1,23 +1,35 @@
 import { useEffect, useRef, useState } from 'react';
 
+/**
+ * Dream - property for a dream
+ * onEdit - property for handleEdit
+ * onDelete - property for handleDelete
+ * @param { dream, onEdit, onDelete } // Properties.
+ * @returns html for a single dream card.
+ */
 export default function DreamCard({ dream, onEdit, onDelete }) {
+  // Initialize edit menu to false.
   const [showMenu, setShowMenu] = useState(false);
-  const menuRef = useRef(null);
+  const menuRef = useRef(null); // Points to menu DOM node.
 
+  // Takes event object from menu button click.
   const toggleMenu = (e) => {
-    e.stopPropagation();
-    setShowMenu((prev) => !prev);
+    e.stopPropagation(); // Prevents onClick handler from firing again and closing the menu.
+    setShowMenu((prev) => !prev); // Flips from whatever the previous state was. Hides and Opens the menu.
   };
 
-  useEffect(() => {
-    const handleClickOutside = (e) => {
+  // Logic to close menu when user clicks outside of it.
+  useEffect(() => { // Runs after the components renders.
+    const handleClickOutside = (e) => { // Triggered whenever the user clicks anywhere on the page.
+      // Checks if menu node is visible and if there was a click anywhere outside of the menu.
       if (menuRef.current && !menuRef.current.contains(e.target)) {
-        setShowMenu(false);
+        setShowMenu(false); // Hide menu if clicked outside.
       }
     };
 
+    // Add event listener for click
     document.addEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside); // Clean up listener to prevent memory stacking.
   }, []);
 
   return (
