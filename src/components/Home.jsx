dd6d600d0ca7a2b5823e6081f1d6e277f86c0bbb
Cole Puls <colepuls@me.com>
2025-06-20 16:11:23 -0500

    Updated documentation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,39 +5,31 @@ import { queryOllama } from '../apis/OllamaApi';
 import { Search } from 'lucide-react';
 import '../assets/Home.css';
 
-/**
- * Home page contains: Dream cards, Search for dream filter, and a page    Navigator.
- */
 export default function Home() {
   const [dreams, setDreams] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredDreams, setFilteredDreams] = useState([]);
 
-  // Runs once (}, []) after the home page component mounts. Loads all of the saved dreams when app is first loaded.
   useEffect(() => {
     const saved = JSON.parse(localStorage.getItem('dreams')) || [];
     setDreams(saved);
     setFilteredDreams(saved);
   }, []);
 
-  // Updates list of dreams in local storage.
   const updateLocalStorage = (updatedDreams) => {
     setDreams(updatedDreams);
     setFilteredDreams(updatedDreams);
     localStorage.setItem('dreams', JSON.stringify(updatedDreams));
   };
 
-  // Filters and gets all dreams that are not the deleted dream. Resulting in deleting a dream. Updates local storage
   const handleDelete = (id) => {
     const filtered = dreams.filter((d) => d.id !== id);
     updateLocalStorage(filtered);
   };
 
-  // Passes id of dream that is being edited, prompts user to edit title.
   const handleEdit = (id) => {
     const newTitle = prompt('Edit title:');
     if (!newTitle) return;
-    // If dream is the edited dream, copy the dream and override the title property with the new title.
     const updated = dreams.map((d) =>
       d.id === id ? { ...d, title: newTitle } : d
     );
@@ -46,7 +38,6 @@ export default function Home() {
 
   
   useEffect(() => {
-    // Timeout waits 600ms before running logic.
     const timeout = setTimeout(async () => {
       if (!searchQuery.trim()) {
         setFilteredDreams(dreams);
@@ -65,7 +56,6 @@ export default function Home() {
         const res = await queryOllama(systemPrompt);
         const keywords = res.split(',').map(k => k.trim().toLowerCase()).filter(k => k);
 
-        // Checks if any keywords from prompt match a dreams title or mood.
         const matches = dreams.filter(dream => {
           const text = `${dream.title} ${dream.mood}`.toLowerCase();
           return keywords.some(k => text.includes(k));
@@ -77,7 +67,7 @@ export default function Home() {
     }, 600);
 
     return () => clearTimeout(timeout);
-  }, [searchQuery, dreams]); // Run the use effect everytime these change.
+  }, [searchQuery, dreams]);
 
   return (
     <div className="page-container">
